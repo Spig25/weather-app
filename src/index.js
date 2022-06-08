9749e16adbef9c99127d7ebcd7e1bd7bedd6a52e
Kasey <kasey.murdoch@yahoo.com>
2022-06-07 17:26:10 -0700

    some styling, can switch between unit metrics

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ const imperialTitle = document.querySelector(`.imperial-title`)
 const metricTitle = document.querySelector(`.metric-title`)
 const imperialDisplay = document.querySelector(`.imperial-unit`)
 const metricDisplay = document.querySelector(`.metric-unit`)
+const humidity = document.querySelector(`.humidity`)
+
+imperialTitle.classList.add(`show`)
+imperialDisplay.classList.add(`show`)
 
 const getWeather = async (city, state, country, units) => {
   try {
@@ -24,12 +28,24 @@ const getWeather = async (city, state, country, units) => {
 const displayWeather = async () => {
   const imperialWeather = await getWeather(cityInput.value, stateInput.value, countryInput.value, `imperial`)
   const metricWeather = await getWeather(cityInput.value, stateInput.value, countryInput.value, `metric`)
-  imperialDisplay.textContent = imperialWeather.main.temp
-  metricDisplay.textContent = metricWeather.main.temp
-  imperialTitle.classList.add(`show`)
-  imperialDisplay.classList.add(`show`)
+  imperialDisplay.textContent = `${Math.round(imperialWeather.main.temp)}F`
+  metricDisplay.textContent = `${Math.round(metricWeather.main.temp)}C`
+  humidity.textContent = `Humidity: ${imperialWeather.main.humidity}%`
 }
 
 button.addEventListener(`click`, () => {
   displayWeather()
+})
+
+metricTitle.addEventListener(`click`, () => {
+  metricTitle.classList.add(`show`)
+  imperialTitle.classList.remove(`show`)
+  imperialDisplay.classList.remove(`show`)
+  metricDisplay.classList.add(`show`)
+})
+imperialTitle.addEventListener(`click`, () => {
+  imperialTitle.classList.add(`show`)
+  metricTitle.classList.remove(`show`)
+  metricDisplay.classList.remove(`show`)
+  imperialDisplay.classList.add(`show`)
 })
